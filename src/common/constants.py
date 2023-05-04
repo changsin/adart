2656dfe57ee68ceb76c28ec4ed912d0c699cbe99
Changsin Lee <changsin@testworks.co.kr>
2023-05-04 18:01:48 +0900

    Updated error types with the latest db changes

--- src/common/constants.py
+++ src/common/constants.py
@@ -25,16 +25,16 @@ PROJECT_COLUMNS = ['id', 'name', 'file_format_id',
 
 TASK_COLUMNS = ['id', 'name', "project_id"]
 
-USER_TYPES = ["admin", "reviewer", "inspector", "user"]
+USER_TYPES = ["user", "reviewer", "inspector", "administrator"]
 
 
 class ErrorType(Enum):
     # DVE_NOERROR = (0, "No error")       # "무오류"
-    DVE_MISS = (1, "mis-tagged")        # "오태깅"
-    DVE_UNTAG = (2, "untagged")        # "미태깅"
-    DVE_OVER = (3, "over-tagged")       # "과태깅"
-    DVE_RANGE = (4, "range error")      # "범위오류"
-    DVE_ATTR = (5, "attributes error")  # "속성오류"
+    DVE_MISS = (1, "Mis-tagged")        # "오태깅"
+    DVE_UNTAG = (2, "Untagged")        # "미태깅"
+    DVE_OVER = (3, "Over-tagged")       # "과태깅"
+    DVE_RANGE = (4, "Range_error")      # "범위오류"
+    DVE_ATTR = (5, "Attributes_error")  # "속성오류"
 
     def __new__(cls, value, description):
         obj = object.__new__(cls)
@@ -46,6 +46,32 @@ class ErrorType(Enum):
     @staticmethod
     def get_all_types():
         return [""] + [error_type.description for error_type in ErrorType]
+
+
+class DomainCode(Enum):
+    DMN_FARM = (1, "Farming")                           # "농,축,수산업"
+    DMN_CULTURE = (2, "Culture")                        # "문화,스포츠,관광
+    DMN_SAFETY = (3, "Safety")                          # "안전,환경"
+    DMN_LANGUAGE = (4, "Language")                      # "음성,자연어
+    DMN_HEALTH = (5, "Health")                          # "의료,건강서비스
+    DMN_TRAFFIC = (6, "Traffic")                        # "자율주행,교통
+    DMN_STRATEGY = (7, "Strategy")                      # "전략"
+    DMN_MANUFACTURE = (8, "Manufacturing")              # "제조,로보틱스
+    DMN_COM_GRAPHICS = (9, "Computer Graphics")         # "컴퓨터 그래픽스
+    DMN_COM_VISION = (10, "Computer Vision")            # "컴퓨터 비전
+    DMN_CONSUMER = (11, "Consumer Products")            # "consumer products
+    DMN_ETC = (12, "Etc.")                              # "기타"
+
+    def __new__(cls, value, description):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.description = description
+
+        return obj
+
+    @staticmethod
+    def get_all_types():
+        return [""] + [domain.description for domain in DomainCode]
 
 
 # Type1
@@ -282,32 +308,6 @@ class TypeRoadMarkerQ(Enum):
     @staticmethod
     def get_all_types():
         return [item.description for item in TypeRoadMarkerQ]
-
-
-class DomainCode(Enum):
-    DMN_FARM = (1, "Farming")   # "농,축,수산업"
-    DMN_CULTURE = (2, "Culture, Sports, Tourism")       # "문화,스포츠,관광
-    DMN_SAFETY = (3, "Safety & Environment")            # "안전,환경"
-    DMN_LANGUAGE = (4, "Language and Voice")            # "음성,자연어
-    DMN_HEALTH = (5, "Medical & Health Services")       # "의료,건강서비스
-    DMN_TRAFFIC = (6, "Autonomous Driving & Traffic")   # "자율주행,교통
-    DMN_STRATEGY = (7, "Strategy")                      # "전략"
-    DMN_MANUFACTURE = (8, "Manufacturing")              # "제조,로보틱스
-    DMN_COM_GRAPHICS = (9, "Computer Graphics")         # "컴퓨터 그래픽스
-    DMN_COM_VISION = (10, "Computer Vision")            # "컴퓨터 비전
-    DMN_CONSUMER = (11, "Consumer Products")            # "consumer products
-    DMN_ETC = (12, "Etc.")                              # "기타"
-
-    def __new__(cls, value, description):
-        obj = object.__new__(cls)
-        obj._value_ = value
-        obj.description = description
-
-        return obj
-
-    @staticmethod
-    def get_all_types():
-        return [""] + [domain.description for domain in DomainCode]
 
 
 class ModelTaskType(Enum):
