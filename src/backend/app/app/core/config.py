42b20d261090281170323cb1899aeca24d844ebb
changsin <changsin@testworks.co.kr>
2023-05-05 09:41:24 +0900

    Reverted refactoring backend due to an error in finding modules

--- src/backend/app/app/core/config.py
+++ src/backend/app/app/core/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     SERVER_NAME: str
     SERVER_HOST: AnyHttpUrl
-    FILE_SERVER_HOST: AnyHttpUrl = "http://192.168.12.79/static"
+    FILE_SERVER_HOST: AnyHttpUrl = "http://192.168.45.172/static"
     # BACKEND_CORS_ORIGINS is a JSON-formatted list of origins
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
